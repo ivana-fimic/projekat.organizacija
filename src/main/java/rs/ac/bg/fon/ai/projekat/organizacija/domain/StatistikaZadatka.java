83f2f74835f768210f110c687e860d493901ce71
ivana-fimic <ek.ivanafimic@gmail.com>
2024-07-09 20:38:52 +0200

    dokumentacija za klasu StatistikaZadatka

--- src/main/java/rs/ac/bg/fon/ai/projekat/organizacija/domain/StatistikaZadatka.java
+++ src/main/java/rs/ac/bg/fon/ai/projekat/organizacija/domain/StatistikaZadatka.java
@@ -16,18 +16,51 @@ import java.util.Objects;
 
 
 /**
- *
+ *Predstavlja statistiku vezanu za izvršavanje zadatka od strane člana tima u kojoj se beleze
+ * faza,datum dodele i krajnji rok zadatka
  * @author Ivana
  */
 public class StatistikaZadatka extends AbstractDomainObject {
 
+    /**
+     * Član tima koji je zadužen za izvršavanje zadatka.
+     */
     private Clan clan;
+
+    /**
+     * Zadatak na koji se statistika odnosi.
+     */
     private Zadatak zadatak;
+
+    /**
+     * Faza zadatka u do koje je clan dosao sa izvršavanjem zadatka.
+     */
     private FazaZadatka FazaZadatka;
+
+    /**
+     * Datum dodele zadatka članu.
+     */
     private LocalDate datumDodele;
+
+    /**
+     * Krajnji rok za izvršavanje zadatka.
+     */
     private LocalDate KrajnjiRok;
+
+    /**
+     * Pomocni parametar vezan za člana (opciono).
+     */
     private String zaClana = "";
 
+    /**
+     * Pravi nov objekat klase StatistikaZadatka sa zadatim vrednostima.
+     * 
+     * @param clan član tima kao Clan
+     * @param zadatak zadatak kao Zadatak
+     * @param FazaZadatka faza zadatka kao FazaZadatka
+     * @param datumDodele datum dodele zadatka kao LocalDate
+     * @param KrajnjiRok krajnji rok za izvršavanje zadatka kao LocalDate
+     */
     public StatistikaZadatka(Clan clan, Zadatak zadatak, FazaZadatka FazaZadatka, LocalDate datumDodele, LocalDate KrajnjiRok) {
         this.clan = clan;
         this.zadatak = zadatak;
@@ -35,89 +68,168 @@ public class StatistikaZadatka extends AbstractDomainObject {
         this.datumDodele = datumDodele;
         this.KrajnjiRok = KrajnjiRok;
     }
-    
 
-    public StatistikaZadatka() {
-    }
+    /**
+     * Pravi nov objekat klase StatistikaZadatka.
+     * 
+     * Polja ostaju neinicijalizovana.
+     */
+    public StatistikaZadatka() {}
 
+    /**
+     * Vraća člana tima koji je zadužen za izvršavanje zadatka.
+     * 
+     * @return član tima kao Clan
+     */
     public Clan getClan() {
         return clan;
     }
 
+    /**
+     * Postavlja člana tima koji je zadužen za izvršavanje zadatka na unetu vrednost.
+     * 
+     * @param clan član tima kao Clan
+     * 
+     * @throws java.lang.NullPointerException 
+     * ako je uneti član null
+     */
     public void setClan(Clan clan) {
-        if(clan==null){
+        if (clan == null)
             throw new NullPointerException("Clan ne sme biti null");
-        }
+        
         this.clan = clan;
     }
 
+    /**
+     * Vraća dodatni parametar vezan za člana.
+     * 
+     * @return dodatni parametar kao String
+     */
     public String getZaClana() {
         return zaClana;
     }
 
+    /**
+     * Postavlja dodatni parametar vezan za člana na unetu vrednost.
+     * 
+     * @param zaClana dodatni parametar kao String
+     */
     public void setZaClana(String zaClana) {
-        
         this.zaClana = zaClana;
     }
 
+    /**
+     * Vraća zadatak na koji se statistika odnosi.
+     * 
+     * @return zadatak kao Zadatak
+     */
     public Zadatak getZadatak() {
-       
         return zadatak;
     }
 
+    /**
+     * Postavlja zadatak na koji se statistika odnosi na unetu vrednost.
+     * 
+     * @param zadatak zadatak kao Zadatak
+     * 
+     * @throws java.lang.NullPointerException 
+     * ako je uneti zadatak null
+     */
     public void setZadatak(Zadatak zadatak) {
-        if(zadatak==null){
+        if (zadatak == null)
             throw new NullPointerException("Naziv zadatka ne sme biti null");
-        }
+        
         this.zadatak = zadatak;
     }
 
+    /**
+     * Vraća fazu zadatka u kojoj se nalazi statistika.
+     * 
+     * @return faza zadatka kao FazaZadatka
+     */
     public FazaZadatka getFazaZadatka() {
         return FazaZadatka;
     }
 
+    /**
+     * Postavlja fazu zadatka na unetu vrednost.
+     * 
+     * @param FazaZadatka faza zadatka kao FazaZadatka
+     * 
+     * @throws java.lang.NullPointerException 
+     * ako je uneta faza zadatka null
+     */
     public void setFazaZadatka(FazaZadatka FazaZadatka) {
-        if(FazaZadatka==null){
-            throw new NullPointerException("Faza  zadatka ne sme biti null");
-        }
+        if (FazaZadatka == null)
+            throw new NullPointerException("Faza zadatka ne sme biti null");
+        
         this.FazaZadatka = FazaZadatka;
     }
 
+    /**
+     * Vraća datum dodele zadatka članu.
+     * 
+     * @return datum dodele kao LocalDate
+     */
     public LocalDate getDatumDodele() {
         return datumDodele;
     }
 
+    /**
+     * Postavlja datum dodele zadatka članu na unetu vrednost.
+     * 
+     * @param datumDodele datum dodele kao LocalDate
+     * 
+     * @throws java.lang.NullPointerException 
+     * ako je uneti datum dodele null
+     */
     public void setDatumDodele(LocalDate datumDodele) {
-         if(datumDodele==null){
+        if (datumDodele == null)
             throw new NullPointerException("Datum dodele ne sme biti null");
-        }
+        
         this.datumDodele = datumDodele;
     }
 
+    /**
+     * Vraća krajnji rok za izvršavanje zadatka.
+     * 
+     * @return krajnji rok kao LocalDate
+     */
     public LocalDate getKrajnjiRok() {
         return KrajnjiRok;
     }
 
+    /**
+     * Postavlja krajnji rok za izvršavanje zadatka na unetu vrednost.
+     * 
+     * @param KrajnjiRok krajnji rok kao LocalDate
+     * 
+     * @throws java.lang.NullPointerException 
+     * ako je uneti krajnji rok null
+     * @throws java.lang.IllegalArgumentException
+     *  ako je uneti krajnji rok u prošlosti
+     */
     public void setKrajnjiRok(LocalDate KrajnjiRok) {
-         if(KrajnjiRok==null){
+        if (KrajnjiRok == null)
             throw new NullPointerException("Krajnji Rok ne sme biti null");
-        }
-         if(KrajnjiRok.isBefore(LocalDate.now())){
-           throw new IllegalArgumentException("Datum ne sme biti u proslosti");
-        }
+        
+        if (KrajnjiRok.isBefore(LocalDate.now()))
+            throw new IllegalArgumentException("Datum ne sme biti u proslosti");
+        
         this.KrajnjiRok = KrajnjiRok;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 59 * hash + Objects.hashCode(this.clan);
-        hash = 59 * hash + Objects.hashCode(this.zadatak);
-        hash = 59 * hash + Objects.hashCode(this.FazaZadatka);
-        hash = 59 * hash + Objects.hashCode(this.datumDodele);
-        hash = 59 * hash + Objects.hashCode(this.KrajnjiRok);
-        return hash;
-    }
+    /**
+     * Poredi dve statistike prema zadatku i clanu.
+     * 
+     * @param obj Druga statistika sa kojim se poredi
+     * 
+     * @return 
+     * true -  ako su oba objekta inicijalizovana, klase su StatistikaZadatka
+     * i imaju istog clana i zadatak 
+     * false - ako nisu klase StatistikaZadatka, ako je uneta statistika null ili ako
+     * nisu isti clan i zadatak
+     */
 
     @Override
     public boolean equals(Object obj) {
@@ -134,21 +246,19 @@ public class StatistikaZadatka extends AbstractDomainObject {
         if (!Objects.equals(this.clan, other.clan)) {
             return false;
         }
-        if (!Objects.equals(this.zadatak, other.zadatak)) {
-            return false;
-        }
-        if (this.FazaZadatka != other.FazaZadatka) {
-            return false;
-        }
-        if (!Objects.equals(this.datumDodele, other.datumDodele)) {
-            return false;
-        }
-        return Objects.equals(this.KrajnjiRok, other.KrajnjiRok);
+        return Objects.equals(this.zadatak, other.zadatak);
     }
 
+    
+    
+     /**
+     * Vraća string reprezentaciju objekta StatistikaZadatka.
+     * 
+     * @return clan, zadatak i fazu kao String
+     */
     @Override
     public String toString() {
-        return clan+" "+zadatak+" "+FazaZadatka;
+        return clan + " " + zadatak + " " + FazaZadatka;
     }
 
     @Override
